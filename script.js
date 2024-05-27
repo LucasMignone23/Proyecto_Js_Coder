11c773b902db46c3dc60facc5baa421ffd32f331
LucasMignone23 <lucasmignone.lm@gmail.com>
2024-05-27 18:21:04 -0300

    Creacion del arreglo con los productos disponibles e iniciacion del array carrito vacio

---
+++ script.js
@@ -0,0 +1,10 @@
+//Creacion de array con los productos disponibles.
+const productos = [
+    { id: 1, nombre: 'Samsung S24 Ultra', precio: 1000 },
+    { id: 2, nombre: 'Samsung Tab 9', precio: 1200 },
+    { id: 3, nombre: 'Samsung SmartWatch', precio: 400 },
+    { id: 4, nombre: 'TV Samsung 50"', precio: 500 }
+];
+
+// Array del carrito
+let carrito = [];
