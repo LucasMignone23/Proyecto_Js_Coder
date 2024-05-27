fcd177807eda3981ffa613b21d7849e2dd08167d
LucasMignone23 <lucasmignone.lm@gmail.com>
2024-05-27 18:41:31 -0300

    arreglo de escritura

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Creacion de array con los productos disponibles.
+//Creacion de array de objetos con los productos disponibles.
 const productos = [
     { id: 1, nombre: 'Samsung S24 Ultra', precio: 1000 },
     { id: 2, nombre: 'Samsung Tab 9', precio: 1200 },
